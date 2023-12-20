d91c2b9e2a5672b02e85e8517bed73e0ac78292f
oals <136543676+oals@users.noreply.github.com>
2023-12-20 12:42:47 +0900

    20231220

--- MyDBApiServer/src/main/java/com/project/MyDBApiServer/service/MyDBApiServiceImpl.java
+++ MyDBApiServer/src/main/java/com/project/MyDBApiServer/service/MyDBApiServiceImpl.java
@@ -86,67 +86,19 @@ public class MyDBApiServiceImpl implements MyDBApiService{
 
         String query = "";
 
-
-
         try {
 
-            if(dbInfoDTO.getDbName().equals("MariaDB")){
+            if(dbInfoDTO.getUrl().contains("jdbc:mariadb://")){
                 query = "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema ='" +  dbInfoDTO.getDbName() + "'";
                 conn = DriverManager.getConnection(dbInfoDTO.getUrl() + dbInfoDTO.getDbName(), dbInfoDTO.getId(), dbInfoDTO.getPswd());
 
-            }else{
+            }else if(dbInfoDTO.getUrl().contains("jdbc:oracle:thin:@")){
                 query = "SELECT table_name FROM user_tables;";
                 conn = DriverManager.getConnection(dbInfoDTO.getUrl(), dbInfoDTO.getId(), dbInfoDTO.getPswd());
 
-                String query2 = "CREATE TABLE employees (" +
-                        "    id NUMBER," +
-                        "    name VARCHAR2(100)," +
-                        "    salary NUMBER," +
-                        "    hire_date DATE" +
-                        ")";
-                Statement stmt = conn.createStatement();
-                stmt.execute(query2);
 
-                String query3 = "CREATE TABLE test (" +
-                        "    id NUMBER," +
-                        "    name VARCHAR2(100)," +
-                        "    salary NUMBER," +
-                        "    hire_date DATE" +
-                        ")";
-                Statement stmt2 = conn.createStatement();
-                stmt2.execute(query3);
-
-
-                String query4 = "CREATE TABLE phoneDB (" +
-                        "    id NUMBER," +
-                        "    name VARCHAR2(100)," +
-                        "    salary NUMBER," +
-                        "    hire_date DATE" +
-                        ")";
-                Statement stmt3 = conn.createStatement();
-                stmt3.execute(query4);
-
-
-                String query5 = "CREATE TABLE playInfo (" +
-                        "    id NUMBER," +
-                        "    name VARCHAR2(100)," +
-                        "    salary NUMBER," +
-                        "    hire_date DATE" +
-                        ")";
-                Statement stmt4 = conn.createStatement();
-                stmt4.execute(query5);
-
-
-                String query6 = "CREATE TABLE money (" +
-                        "    id NUMBER," +
-                        "    name VARCHAR2(100)," +
-                        "    salary NUMBER," +
-                        "    hire_date DATE" +
-                        ")";
-                Statement stmt5 = conn.createStatement();
-                stmt5.execute(query6);
-
-
+            }else{
+                log.info("오류");
             }
 
 
@@ -183,27 +135,44 @@ public class MyDBApiServiceImpl implements MyDBApiService{
         List<String> columnNameList = new ArrayList<>();
         List<List<String>> dataList = new ArrayList<>();
 
-        try {
-            conn = DriverManager.getConnection(dbInfoDTO.getUrl() + dbInfoDTO.getDbName() , dbInfoDTO.getId(), dbInfoDTO.getPswd());
 
+        String query = "";
+        String query2 = "";
+
+
+        try {
+            if(dbInfoDTO.getUrl().contains("jdbc:mariadb://")){
+                conn = DriverManager.getConnection(dbInfoDTO.getUrl() + dbInfoDTO.getDbName() , dbInfoDTO.getId(), dbInfoDTO.getPswd());
+                query = "SELECT column_name FROM information_schema.columns WHERE table_name ='"  + dbInfoDTO.getTableName() + "'";
+                query2 = "SELECT * from " + dbInfoDTO.getTableName();
+
+
+            }else if(dbInfoDTO.getUrl().contains("jdbc:oracle:thin:@")){
+                conn = DriverManager.getConnection(dbInfoDTO.getUrl(), dbInfoDTO.getId(), dbInfoDTO.getPswd());
+                query = "SELECT column_name FROM all_tab_columns WHERE table_name ='"  + dbInfoDTO.getTableName().toUpperCase() + "'";
+                query2 = "SELECT * FROM " + dbInfoDTO.getTableName();
+            }else{
+                log.info("오류");
+            }
         } catch (SQLException e) {
 
         }
 
-        String query = "SELECT column_name FROM information_schema.columns WHERE table_name ='"  + dbInfoDTO.getTableName() + "'";
-        String query2 = "SELECT * from " + dbInfoDTO.getTableName();
+
+
 
         try {
             Statement stmt = conn.createStatement();
             ResultSet columnNameResult = stmt.executeQuery(query);
+
             while (columnNameResult.next()) {
                 String columnName = columnNameResult.getString("column_name");
                 columnNameList.add(columnName);
             }
 
             dataList.add(columnNameList);
-
             ResultSet tableDataResult = stmt.executeQuery(query2);
+
             while (tableDataResult.next()) {
                 List<String> dataList2 = new ArrayList<>();
 
